cd06256eb44195a34bee9e5e7604440180f86fab
NITIN GOSWAMI <sanyamgoswami21@gmail.com>
2019-04-19 14:43:41 +0530

    Commit for ps4 autoload.

--- src/Test/Test.php
+++ src/Test/Test.php
@@ -1,12 +1,9 @@
-<?php
+<?php 
 namespace Wepro;
-
-class Test {
-	
-	public static function test(){
-		return 'hello';
-	}
+class Test
+{
+    public static function test()
+    {
+        return 'Hello World, Composer!';
+    }
 }
-
-
-?>
